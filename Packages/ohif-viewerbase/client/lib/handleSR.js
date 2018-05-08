40ae48238b431477787152e1dc6dc92262ce1b58
Steve Pieper <pieper@isomics.com>
2018-05-08 09:25:59 -0400

    Fixes reload of multiple SRs with length measurements
    Now the loaded lengths are added to theexisting tool state
    and multiple lengths per SR are supported on unique frames
    (either unique instances or frames of a multiframe).
    
    This requires SRs created using the updated stowSR function
    provided in this gist:
    
    https://gist.github.com/pieper/e025f0299b326efb3671cb3e5b196f13

--- Packages/ohif-viewerbase/client/lib/handleSR.js
+++ Packages/ohif-viewerbase/client/lib/handleSR.js
@@ -5,14 +5,33 @@ function loadSRTools(Part10SRArrayBuffer, displaySets) {
 
     let dataset = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
 
+    console.log(dataset);
+
     // add the length measurement to cornerstoneTools
-    let toolStateManager = cornerstoneTools.globalImageIdSpecificToolStateManager;
+    const toolStateManager = cornerstoneTools.globalImageIdSpecificToolStateManager;
 
-    let toolState = imagingMeasurementsToToolState(dataset, displaySets);
+    // convert the SR into the kind of toolState cornerstoneTools needs
+    const loadedToolState = imagingMeasurementsToToolState(dataset, displaySets);
 
-    if (toolState != undefined) {
-      toolStateManager.restoreToolState(toolState);
+
+    // merge the loadedToolState to the existing toolState
+    const existingToolState = toolStateManager.saveToolState();
+    if (Object.keys(existingToolState).length == 0) {
+      existingToolState = {};
     }
+    Object.keys(loadedToolState).forEach(imageId => {
+      if (!existingToolState[imageId]) {
+        existingToolState[imageId] = {};
+      }
+      if (!existingToolState[imageId].length) {
+        existingToolState[imageId].length = {data: []};
+      }
+      loadedToolState[imageId].length.data.forEach(lengthData => {
+        console.log(`adding`, lengthData);
+        existingToolState[imageId].length.data.push(lengthData);
+      });
+    });
+    toolStateManager.restoreToolState(existingToolState);
 }
 
 function imagingMeasurementsToToolState(dataset, displaySets) {
@@ -21,17 +40,18 @@ function imagingMeasurementsToToolState(dataset, displaySets) {
     // TODO: generalize to the kinds of measurements the Viewer supports
     if (dataset.ContentTemplateSequence.TemplateIdentifier !== "1500") {
         console.warn("This code can only interpret TID 1500");
-        return(undefined);
+        return({});
     }
     toArray = function(x) { return (x.constructor.name === "Array" ? x : [x]); }
 
-    let lengthState = {};
+    let lengthStates = [];
     toArray(dataset.ContentSequence).forEach(contentItem => {
         if (contentItem.ConceptNameCodeSequence.CodeMeaning === "Imaging Measurements") {
             toArray(contentItem.ContentSequence).forEach(measurementContent => {
                 if (measurementContent.ConceptNameCodeSequence.CodeMeaning === "Measurement Group") {
                     toArray(measurementContent.ContentSequence).forEach(groupItemContent => {
                         if (groupItemContent.ConceptNameCodeSequence.CodeMeaning === "Length") {
+                            let lengthState = {};
                             lengthState.measuredValue = groupItemContent.MeasuredValueSequence.NumericValue;
                             const lengthContent = groupItemContent.ContentSequence;
                             lengthState.handles = {start: {}, end: {}};
@@ -42,93 +62,104 @@ function imagingMeasurementsToToolState(dataset, displaySets) {
                             const reference = lengthContent.ContentSequence.ReferencedSOPSequence;
                             lengthState.ReferencedInstanceUID = reference.ReferencedSOPInstanceUID;
                             lengthState.ReferencedFrameNumber = reference.ReferencedFrameNumber;
+                            lengthStates.push(lengthState);
                         }
                     });
                 }
             });
         }
     });
+    console.log(lengthStates);
 
     // TODO: determine correct imageId from
     // lengthState.ReferencedInstanceUID and lengthState.ReferencedFrameNumber
     // find imageIds for all viewers
 
-    let imageId;
+    let lengthStatesByImageId = {};
 
-    displaySets.forEach(displaySet => {
-        displaySet.images.forEach(instanceMetadata => {
-            if (lengthState.ReferencedInstanceUID === instanceMetadata._sopInstanceUID) {
-                imageId = instanceMetadata.getImageId();
-                const frame = lengthState.ReferencedFrameNumber
-                if (frame != undefined && !isNaN(frame)) { // add or update a frame parameter if needed
-                    const frameParameter = `&frame=${lengthState.ReferencedFrameNumber}`;
-                    if (imageId.indexOf('frame=') == -1) {
-                        imageId += frameParameter;
-                    } else {
-                        imageId.replace(/frame=[0-9]*/, frameParameter);
-                    }
-                }
-            }
-        });
+    lengthStates.forEach(lengthState => {
+      let imageId;
+      displaySets.forEach(displaySet => {
+          displaySet.images.forEach(instanceMetadata => {
+              if (lengthState.ReferencedInstanceUID === instanceMetadata._sopInstanceUID) {
+                  imageId = instanceMetadata.getImageId();
+                  const frame = lengthState.ReferencedFrameNumber
+                  if (frame != undefined && !isNaN(frame)) { // add or update a frame parameter if needed
+                      const frameParameter = `&frame=${lengthState.ReferencedFrameNumber}`;
+                      if (imageId.indexOf('frame=') == -1) {
+                          imageId += frameParameter;
+                      } else {
+                          imageId.replace(/frame=[0-9]*/, frameParameter);
+                      }
+                  }
+              }
+          });
+      });
+      if (!imageId) {
+        console.warn(`No imageId for ${lengthState.ReferencedInstanceUID}`);
+        console.log(dataset);
+      }
+      lengthStatesByImageId[imageId] = lengthState;
     });
 
-    if (!imageId) {
-        console.warn(`No imageId for ${lengthState.ReferencedInstanceUID}`);
-        return;
-    }
 
-    // TODO: populate the measurments schema?
-    const toolState = `
-    { 
-      "${imageId}": {
-        "length": {
-          "data": [
-            {
-              "visible": true,
-              "active": false,
-              "handles": {
-                "start": {
-                  "x": ${lengthState.handles.start.x},
-                  "y": ${lengthState.handles.start.y},
-                  "highlight": true,
-                  "active": false
-                },
-                "end": {
-                  "x": ${lengthState.handles.end.x},
-                  "y": ${lengthState.handles.end.y},
-                  "highlight": true,
-                  "active": false
-                },
-                "textBox": {
-                  "active": false,
-                  "hasMoved": false,
-                  "movesIndependently": false,
-                  "drawnIndependently": true,
-                  "allowedOutsideImage": true,
-                  "hasBoundingBox": true,
-                  "x": 109.60703812316717,
-                  "y": 123.87096774193549,
-                  "boundingBox": {
-                    "width": 77.646484375,
-                    "height": 25,
-                    "left": 680,
-                    "top": 317.5
-                  }
-                }
-              },
-              "length": ${lengthState.measuredValue},
-              "invalidated": true
+    let toolState = {};
+    Object.keys(lengthStatesByImageId).forEach(imageId => {
+      let lengthState = lengthStatesByImageId[imageId];
+      const lengthData = JSON.parse(`
+        {
+        "visible": true,
+        "active": false,
+        "handles": {
+          "start": {
+            "x": ${lengthState.handles.start.x},
+            "y": ${lengthState.handles.start.y},
+            "highlight": true,
+            "active": false
+          },
+          "end": {
+            "x": ${lengthState.handles.end.x},
+            "y": ${lengthState.handles.end.y},
+            "highlight": true,
+            "active": false
+          },
+          "textBox": {
+            "active": false,
+            "hasMoved": false,
+            "movesIndependently": false,
+            "drawnIndependently": true,
+            "allowedOutsideImage": true,
+            "hasBoundingBox": true,
+            "x": 109.60703812316717,
+            "y": 123.87096774193549,
+            "boundingBox": {
+              "width": 77.646484375,
+              "height": 25,
+              "left": 680,
+              "top": 317.5
             }
-          ]
-        }
+          }
+        },
+        "length": ${lengthState.measuredValue},
+        "invalidated": true
       }
-    }`
-    return JSON.parse(toolState);
+    `);
+    if (!toolState[imageId]) {
+      toolState[imageId] = {length : {data: []}};
+    }
+    console.log('lengthData', lengthData);
+    toolState[imageId].length.data.push(lengthData);
+  });
+
+  return toolState;
 }
 
 export function handleSR(series, displaySets) {
 
     const instance = series.getFirstInstance();
+
+    console.log(`loading ${instance}`);
+
 
     let request = new XMLHttpRequest();
     request.responseType = 'arraybuffer';
