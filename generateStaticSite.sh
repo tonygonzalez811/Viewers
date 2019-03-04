0d976954fb2cc65d455e87d0499005dc3f1aabdd
Erik Ziegler <erik.sweed@gmail.com>
2019-03-04 12:51:53 +0100

    WIP on React docs

--- generateStaticSite.sh
+++ generateStaticSite.sh
@@ -2,11 +2,42 @@ cd docs
 yarn -v
 node -v
 echo 'Installing Gitbook CLI'
-yarn install
+yarn global add gitbook-cli
 
 echo 'Running Gitbook installation'
-./node_modules/gitbook-cli/bin/gitbook.js install
-./node_modules/gitbook-cli/bin/gitbook.js build
+
+# Generate all version's GitBook output
+for D in *; do
+    if [ -d "${D}" ]; then
+        echo "Generating output for: ${D}"
+		cd "${D}"
+
+		# Clear previous output, generate new
+		rm -rf _book
+		gitbook install
+		gitbook build
+
+		cd ..
+    fi
+done
+
+# Move CNAME File into `latest`
+cp CNAME ./latest/CNAME
+
+# Create a history folder in our latest version's output
+mkdir ./latest/_book/history
+
+# Move each version's files to latest's history folder
+for D in *; do
+	if [ -d "${D}" ]; then
+		if [[ "${D}" == v* ]] ; then
+    		echo "Moving ${D} to the latest version's history folder"
+
+			mkdir "./latest/_book/history/${D}"
+			mv -v "./${D}/_book"/* "./latest/_book/history/${D}"
+		fi
+	fi
+done
 cd ..
 
 # Build and copy the StandaloneViewer into the static directory
